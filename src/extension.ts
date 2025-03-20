94ff680d26213a15bb001120fe026edb9fc195dd
Felix Uhl <github@mail.felix-uhl.de>
2025-03-20 21:00:11 +0100

    Use first or "math" group to identify formula
    The previous mechanic with skipping backward and forward a certain
    number of characters was unreliable and limiting.
    Also the "wrong usage" regex is unnecessary.

--- src/extension.ts
+++ src/extension.ts
@@ -4,9 +4,6 @@ import { Tex2Svg } from "./tex2svg";
 
 export function activate(context: vscode.ExtensionContext) {
 
-	const matchRegEx = new RegExp(config.matchReg, config.matchRegFlags);
-	const wrongRegEx = new RegExp(config.wrongReg, config.wrongRegFlags);
-
 	const tex2Svg = new Tex2Svg(config.cacheSize);
 	let timeout: NodeJS.Timer | undefined = undefined;
 	let activeEditor = vscode.window.activeTextEditor;
@@ -15,22 +12,30 @@ export function activate(context: vscode.ExtensionContext) {
 		if (!activeEditor) {
 			return;
 		}
+
+		const matchRegEx = new RegExp(config.matchReg, config.matchRegFlags);
+
 		const text = activeEditor.document.getText();
 		const wrongUsage: vscode.DecorationOptions[] = [];
 		const correctUsage: vscode.DecorationOptions[] = [];
 		let match;
 		while ((match = matchRegEx.exec(text))) {
-			const start = match.index + config.forwardSkip;
-			const end = match.index + match[0].length - config.backwardSkip;
+			const group1 = match[1];
+			const groupMath = match.groups?.math;
+			const latex = groupMath ?? group1;
+
+			const start = match.index;
+			const end = match.index + match[0].length;
 			const startPos = activeEditor.document.positionAt(start);
 			const endPos = activeEditor.document.positionAt(end);
 			const range = new vscode.Range(startPos, endPos);
-			const latex = match[0].substring(config.forwardSkip, match[0].length - config.backwardSkip);
-			// console.log(`Match: ${match[0]}, match index: ${match.index}, match length: ${match[0].length}, processed: ${latex}`);
+
+			console.log(`Match: ${match[0]}, match index: ${match.index}, match length: ${match[0].length}, processed: ${latex}`);
+
 			const hoverUri = await tex2Svg.render(latex);
 			const decoration = { range: range, hoverMessage: hoverUri.text };
 
-			if (hoverUri.error || wrongRegEx.test(match[0])) {
+			if (hoverUri.error) {
 				wrongUsage.push(decoration);
 			} else {
 				correctUsage.push(decoration);
